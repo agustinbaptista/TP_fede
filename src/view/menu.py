2ba1bd86aab5dde522ea929e5864837cd6789191
Agustin Baptista <83555620+agustinbaptista@users.noreply.github.com>
2023-10-26 00:01:59 -0300

    Delete src directory

--- src/view/menu.py
+++
@@ -1,8 +0,0 @@
-def menu():
-    print("Indique las caracteristicas del celular que quiere comprar ")
-    
-    marca = input("Ingresa la marca del teléfono: ")
-    modelo = input("Ingresa el modelo del teléfono: ")
-    color = input("Ingresa el color del teléfono: ")
-
-    return marca, modelo, color
