2fac7e279046632433f14dfafb80ce740bc6654f
Agustin Baptista <83555620+agustinbaptista@users.noreply.github.com>
2023-11-08 20:51:43 -0300

    Create telefono_handler.py

---
+++ src/service/telefono_handler.py
@@ -0,0 +1,12 @@
+from domain.telefono_abstract import AbstractHandler
+
+class Handler(AbstractHandler):
+    _next_handler = None
+    
+    def set_next(self, handler):
+        self._next_handler = handler
+        return handler
+
+    def handle(self, request):
+        if self._next_handler:
+            return self._next_handler.handle(request)
